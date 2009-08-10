96492ff60247ca93f3c65eea67fe894ea39ec7d0
lboynton <notlee@gmail.com>
2009-08-10 17:48:36 +0100

    Updated DataLoader to support reading of arbitrary paths

--- src/heartsim/DataLoader.java
+++ src/heartsim/DataLoader.java
@@ -8,6 +8,8 @@ import java.awt.geom.GeneralPath;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
@@ -29,14 +31,45 @@ public class DataLoader
     private double size; // cell size (smaller cell size means bigger heart)
     private Document doc;
     private ExtendedGeneralPath ventriclesPath;
+    private boolean fileOpened = false;
+    private ArrayList<String> paths = new ArrayList<String>();
+    private ArrayList<ExtendedGeneralPath> pathShapes = new ArrayList<ExtendedGeneralPath>();
+    private int sizeX = 0;
+    private int sizeY = 0;
 
     public DataLoader()
     {
     }
-    
+
     public DataLoader(String file)
     {
         this.file = file;
+    }
+
+    public DataLoader(String file, String[] paths)
+    {
+        this.file = file;
+        this.paths.addAll(Arrays.asList(paths));
+    }
+
+    public DataLoader(String[] paths)
+    {
+        this.paths.addAll(Arrays.asList(paths));
+    }
+
+    public void addPath(String path)
+    {
+        paths.add(path);
+    }
+
+    public void removePath(String path)
+    {
+        paths.remove(path);
+    }
+
+    public String[] getPaths()
+    {
+        return (String[]) paths.toArray();
     }
 
     public void setSize(double size)
@@ -47,17 +80,21 @@ public class DataLoader
 
     public void setSize(int width, int height)
     {
-        
     }
 
     public void setFile(String file)
     {
         this.file = file;
+        fileOpened = false;
     }
 
     private void openFile()
     {
-        
+        if (fileOpened)
+        {
+            return;
+        }
+
         String parser = XMLResourceDescriptor.getXMLParserClassName();
         SAXSVGDocumentFactory documentFactory = new SAXSVGDocumentFactory(parser);
         String uri = new File(file).toURI().toString();
@@ -101,26 +138,110 @@ public class DataLoader
         // work out how big the 2D cells array should be and add two because we
         // are dividing doubles which will return a decimal value which needs to
         // be rounded up
-        int sizeX = (int) ((int) (ventriclesPath.getBounds2D().getMaxY() / size) - (ventriclesPath.getBounds2D().getMinY() / size)) + 2;
-        int sizeY = (int) ((int) (ventriclesPath.getBounds2D().getMaxX() / size) - (ventriclesPath.getBounds2D().getMinX() / size)) + 2;
+        sizeX = (int) ((int) (ventriclesPath.getBounds2D().getMaxY() / size) - (ventriclesPath.getBounds2D().getMinY() / size)) + 2;
+        sizeY = (int) ((int) (ventriclesPath.getBounds2D().getMaxX() / size) - (ventriclesPath.getBounds2D().getMinX() / size)) + 2;
 
         cells = new boolean[sizeX][sizeY];
 
-        for(double y = ventriclesPath.getBounds2D().getMinY(); y < ventriclesPath.getBounds2D().getMaxY(); y = y + size)
+        for (double y = ventriclesPath.getBounds2D().getMinY(); y < ventriclesPath.getBounds2D().getMaxY(); y = y + size)
         {
-            for(double x = ventriclesPath.getBounds2D().getMinX(); x < ventriclesPath.getBounds2D().getMaxX(); x = x + size)
+            for (double x = ventriclesPath.getBounds2D().getMinX(); x < ventriclesPath.getBounds2D().getMaxX(); x = x + size)
             {
                 cells[xVal][yVal++] = ventriclesPath.contains(x, y);
             }
 
-            yVal=0;
+            yVal = 0;
             xVal++;
+        }
+    }
+
+    private void loadPaths()
+    {
+        ExtendedGeneralPath shape;
+
+        for (String path : paths)
+        {
+            Element element = (Element) doc.getElementById(path);
+
+            if (element == null)
+            {
+                System.out.println("Could not find element: " + path);
+                continue;
+            }
+
+            try
+            {
+                shape = (ExtendedGeneralPath) AWTPathProducer.createShape(new StringReader(element.getAttributeNS(null, "d")), GeneralPath.WIND_EVEN_ODD);
+
+                if (shape != null)
+                {
+                    pathShapes.add(shape);
+                }
+            }
+            catch (IOException ex)
+            {
+                Logger.getLogger(DataLoader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            catch (ParseException ex)
+            {
+                Logger.getLogger(DataLoader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void loadData()
+    {
+        cells = new boolean[sizeX][sizeY];
+
+        for (ExtendedGeneralPath path : pathShapes)
+        {
+            int xVal = 0;
+            int yVal = 0;
+
+            for (double y = path.getBounds2D().getMinY(); y < path.getBounds2D().getMaxY(); y = y + size)
+            {
+                for (double x = path.getBounds2D().getMinX(); x < path.getBounds2D().getMaxX(); x = x + size)
+                {
+                    cells[xVal][yVal++] = path.contains(x, y);
+                }
+
+                yVal = 0;
+                xVal++;
+            }
+        }
+    }
+
+    private void computeSize()
+    {
+        for (ExtendedGeneralPath path : pathShapes)
+        {
+            // work out how big the 2D cells array should be and add two because we
+            // are dividing doubles which will return a decimal value which needs to
+            // be rounded up
+            int maxX = (int) ((int) (path.getBounds2D().getMaxY() / size) - (path.getBounds2D().getMinY() / size)) + 2;
+            int maxY = (int) ((int) (path.getBounds2D().getMaxX() / size) - (path.getBounds2D().getMinX() / size)) + 2;
+
+            // store the largest X and Y value as the size
+            if (maxX > sizeX)
+            {
+                sizeX = maxX;
+            }
+            if (maxY > sizeY)
+            {
+                sizeY = maxY;
+            }
         }
     }
 
     public boolean[][] getGrid()
     {
-        if(cells == null) createGrid();
+        if (cells == null)
+        {
+            openFile();
+            loadPaths();
+            computeSize();
+            loadData();
+        }
         return cells;
     }
 
@@ -146,7 +267,11 @@ public class DataLoader
 
     public static void main(String args[])
     {
-        final DataLoader loader = new DataLoader("./geometry_data/heart.svg");
+        final DataLoader loader = new DataLoader("./geometry_data/heart2.svg", new String[]
+                {
+                    "ventricles",
+                    "atria"
+                });
         loader.setSize(10);
         DataLoader.printGrid(loader.getGrid());
     }
