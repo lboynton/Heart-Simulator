220ac09e8e6c61e50b5012620ef9a283b1360ab3
lboynton <notlee@gmail.com>
2009-08-12 15:48:09 +0100

    Use anti-aliasing to paint heart outline

--- src/heartsim/gui/BinaryPlotPanel.java
+++ src/heartsim/gui/BinaryPlotPanel.java
@@ -13,6 +13,7 @@ package heartsim.gui;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
@@ -91,6 +92,8 @@ public class BinaryPlotPanel extends javax.swing.JPanel
     public void paint(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         g2.drawImage(buffIm, 0, 0, this);
         if (paths != null)
         {
