c3f7ca00a437721aa02f176335a55a920e866220
lboynton <notlee@gmail.com>
2009-08-26 22:02:41 +0100

    Added missing profiles from last commit

---
+++ src/heartsim/cam/profile/BundleOfHis.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heartsim.cam.profile;
+
+import heartsim.cam.CellularAutomataModel;
+import heartsim.cam.Nishiyama;
+
+/**
+ *
+ * @author Lee Boynton
+ */
+public class BundleOfHis extends Profile
+{
+    public BundleOfHis()
+    {
+        super("Bundle of His");
+
+        this.addAlias("AV bundle");
+        this.addAlias("Atrioventricular bundle");
+    }
+
+    @Override
+    public CellularAutomataModel loadParameters(CellularAutomataModel model)
+    {
+        if(model instanceof Nishiyama)
+        {
+            model.getParameter("Delta 1").setValue("3");
+            model.getParameter("Delta 2").setValue("7");
+            model.getParameter("N").setValue("6");
+        }
+
+        return model;
+    }
+}
---
+++ src/heartsim/cam/profile/PurkinjeFibres.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package heartsim.cam.profile;
+
+import heartsim.cam.CellularAutomataModel;
+import heartsim.cam.Nishiyama;
+
+/**
+ *
+ * @author Lee Boynton
+ */
+public class PurkinjeFibres extends Profile
+{
+    public PurkinjeFibres()
+    {
+        super("Purkinje fibres");
+
+        this.addAlias("Purkinje fibers");
+        this.addAlias("Purkyne fibres");
+        this.addAlias("Purkyne fibers");
+        this.addAlias("Subendocardial branches");
+    }
+
+    @Override
+    public CellularAutomataModel loadParameters(CellularAutomataModel model)
+    {
+        if(model instanceof Nishiyama)
+        {
+            model.getParameter("Delta 1").setValue("1");
+            model.getParameter("Delta 2").setValue("2");
+            model.getParameter("N").setValue("3");
+        }
+
+        return model;
+    }
+}
