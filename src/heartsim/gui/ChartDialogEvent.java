1866e6fd4ccbc8f7650002f596037ae12596c037
lboynton <notlee@gmail.com>
2009-08-27 18:55:29 +0100

    Updated todo and added missing class from previous commit

---
+++ src/heartsim/gui/ChartDialogEvent.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heartsim.gui;
+
+/**
+ *
+ * @author Lee Boynton
+ */
+public interface ChartDialogEvent
+{
+    public void setCellSelectionMode();
+    public void cancelCellSelectionMode();
+}
