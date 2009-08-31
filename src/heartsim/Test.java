071372eaac8bbc60323b0ee4c5e9a78bd316566f
lboynton <notlee@gmail.com>
2009-08-31 21:19:44 +0100

    Added code for getting number of available CPU cores

---
+++ src/heartsim/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heartsim;
+
+/**
+ *
+ * @author Lee Boynton
+ */
+public class Test
+{
+    public static void main(String args[])
+    {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
