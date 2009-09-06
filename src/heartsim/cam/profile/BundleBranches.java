e7240d02cd0177488a1553bea4fd3992fa559c8a
lboynton <notlee@gmail.com>
2009-09-06 16:21:13 +0100

    Added aliases and set parameters to Bundle Branches profile

--- src/heartsim/cam/profile/BundleBranches.java
+++ src/heartsim/cam/profile/BundleBranches.java
@@ -2,11 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package heartsim.cam.profile;
 
 import heartsim.cam.CellularAutomataModel;
 import heartsim.cam.Nishiyama;
+import heartsim.cam.NishiyamaExtended;
 
 /**
  *
@@ -20,16 +20,25 @@ public class BundleBranches extends Profile
 
         this.addAlias("Left and right bundle branches");
         this.addAlias("Right and left bundle branches");
+        this.addAlias("Left bundle branch");
+        this.addAlias("Right bundle branch");
+        this.addAlias("Bundle branch");
     }
 
     @Override
     public CellularAutomataModel loadParameters(CellularAutomataModel model)
     {
-        if(model instanceof Nishiyama)
+        if (model instanceof Nishiyama)
         {
-            model.getParameter("Delta 1").setValue("3");
-            model.getParameter("Delta 2").setValue("7");
-            model.getParameter("N").setValue("6");
+            model.getParameter("Delta 1").setValue("30");
+            model.getParameter("Delta 2").setValue("40");
+            model.getParameter("N").setValue("200");
+        }
+
+        if (model instanceof NishiyamaExtended)
+        {
+            model.getParameter("uUp").setValue("105");
+            model.getParameter("uDown").setValue("30");
         }
 
         return model;
